c7f3e04f9135e2585be2d9c75d64a3ad2cf74e80
ayhamdw <s12029113@stu.najah.edu>
2025-04-19 16:31:58 +0300

    Structur the folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
 import React from "react";
+import "./index.css";
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import { RouterProvider } from "react-router";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+  { path: "order/:orderId", element: <Order /> },
+]);
 
 function App() {
-  const x = 15;
-  const y = 10;
-  return <div>Hello Vite</div>;
+  return <RouterProvider router={router} />;
 }
 export default App;
